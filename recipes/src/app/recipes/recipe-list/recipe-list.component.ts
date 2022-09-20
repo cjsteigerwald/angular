9b95711bfb0e1f9ab0094fd2f46357c6d20f55cd
Christopher Steigerwald <cjsteigerwald@gmail.com>
2022-09-20 03:45:12 -0700

    Initial structure complete

--- recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
