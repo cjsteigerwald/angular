053322f4be1423a92c753ed91ad2a802d61f903c
Christopher Steigerwald <cjsteigerwald@gmail.com>
2022-09-20 03:48:29 -0700

    Recipes (#1)
    * Finished basics
    
    * Initial project setup
    
    * Finishing setup of header
    
    * Initial structure complete
    
    * removing my-first-app from branch

--- recipes/README.md
+++ recipes/README.md
@@ -1,4 +1,4 @@
-# MyFirstApp
+# Recipes
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.1.1.
 
---
+++ recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Recipe } from './../recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
