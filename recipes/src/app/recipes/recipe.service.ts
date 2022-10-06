d375e33fd3233044753909edc045513b758716b7
Christopher Steigerwald <cjsteigerwald@gmail.com>
2022-10-06 00:22:35 -0700

    Adding route resolver

--- recipes/src/app/recipes/recipe.service.ts
+++ recipes/src/app/recipes/recipe.service.ts
@@ -7,26 +7,31 @@ import { Recipe } from "./recipe.model";
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
+  private recipes: Recipe[] = [];
 
-  private recipes: Recipe[] = [
-    new Recipe('Shrimp Recipe', 
-      'Delicious Shrimp Dish', 
-      'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg', 
-      [
-        new Ingredient('Zuccinni', 1),
-        new Ingredient('Shrimp', 5)
-      ]),
-    new Recipe('Yummy Burger', 
-      'Best Burger Ever', 
-      'https://media-cdn.tripadvisor.com/media/photo-s/17/90/52/45/2-lb-2-slice-cheese-caramelize.jpg',
-       [
-        new Ingredient('Meat', 1),
-        new Ingredient('Buns', 1)
-       ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe('Shrimp Recipe', 
+  //     'Delicious Shrimp Dish', 
+  //     'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg', 
+  //     [
+  //       new Ingredient('Zuccinni', 1),
+  //       new Ingredient('Shrimp', 5)
+  //     ]),
+  //   new Recipe('Yummy Burger', 
+  //     'Best Burger Ever', 
+  //     'https://media-cdn.tripadvisor.com/media/photo-s/17/90/52/45/2-lb-2-slice-cheese-caramelize.jpg',
+  //      [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Buns', 1)
+  //      ])
+  // ];
 
   constructor(private slService: ShoppingListService) {}
 
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     // returns new array that is copy
