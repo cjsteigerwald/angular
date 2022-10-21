88ccf25d19f315f1923e3b893bfb83824c5f4ed7
Christopher Steigerwald <cjsteigerwald@gmail.com>
2022-10-21 12:22:35 -0700

    adding state for shopping-list module

--- recipes/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipes/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -17,36 +17,35 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   @ViewChild('f', {static: false}) slForm: NgForm;
   subscription: Subscription;
   editMode = false;
-  editedItemIndex: number;
   editedItem: Ingredient;
   constructor(private slService: ShoppingListService, private store: Store<fromShoppingList.AppState>) { }
 
   ngOnInit(): void {
-    this.subscription = this.slService.startedEditing.subscribe(
-      (index: number) => {
-        this.editedItemIndex = index;
+    this.subscription = this.store.select('shoppingList').subscribe(stateData => {
+      if (stateData.editedIngredientIndex > -1) {
         this.editMode = true;
-        this.editedItem = this.slService.getIngredient(index);
+        this.editedItem = stateData.editedIngredient;
         this.slForm.setValue({
           name: this.editedItem.name,
           amount: this.editedItem.amount,
         });
+      } else {
+        this.editMode = false;
       }
-    );
+    });
   }
 
   ngOnDestroy(): void {
-      this.subscription.unsubscribe();
+    this.store.dispatch(new ShoppinListActions.StopEdit());
+    this.subscription.unsubscribe();
   }
 
   onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
-      this.store.dispatch(new ShoppinListActions.UpdateIngredient({index: this.editedItemIndex, ingredient: newIngredient }));
-      // this.slService.updateIngredient(this.editedItemIndex, newIngredient)
+      this.store.dispatch(new ShoppinListActions.UpdateIngredient(newIngredient));
     } else {
-      // this.slService.addIngredient(newIngredient);
       this.store.dispatch(new ShoppinListActions.AddIngredient(newIngredient));
     }
     this.editMode = false;
@@ -54,14 +53,14 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   onDelete() {
-    // this.slService.deleteIngredient(this.editedItemIndex);
-    this.store.dispatch(new ShoppinListActions.DeleteIngredient(this.editedItemIndex));
+    this.store.dispatch(new ShoppinListActions.DeleteIngredient());
     this.onClear();
   }
 
   onClear() {
     this.editMode = false;
     this.slForm.reset();
+    this.store.dispatch(new ShoppinListActions.StopEdit());
   }
 
 
--- recipes/src/app/shopping-list/shopping-list.component.ts
+++ recipes/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,9 @@
-import { ShoppingListService } from './shopping-list.service';
 import * as fromShoppingList from './store/shopping-list.reducer';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
-import { Observable, Subscription } from 'rxjs';
-import { LoggingService } from '../logging.service';
+import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
+import * as ShoppingListActions from './store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-list',
@@ -13,30 +12,20 @@ import { Store } from '@ngrx/store';
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Observable<{ ingredients: Ingredient[] }>;
-  // private subscription: Subscription
   constructor(
-    private slService: ShoppingListService, 
-    // private loggingService: LoggingService, 
     private store: Store<fromShoppingList.AppState>
   ) {} // constructor
 
   ngOnInit(): void {
     this.ingredients = this.store.select('shoppingList');
-    // this.ingredients = this.slService.getIngredients();
-    // this.subscription = this.slService.ingredientsChanged.subscribe(
-    //   (ingredients: Ingredient[]) => {
-    //     this.ingredients = ingredients;
-    //   }
-    // );
-    // this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit');
+
   }
 
   ngOnDestroy(): void {
-    // this.subscription.unsubscribe();    
   }
 
   onEditItem(index: number) {
-    this.slService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
   }
 
 }
